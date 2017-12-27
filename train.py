b81ccef5f1047426a71737f95bcdcd9219b083ef
paras <mail2paras.s@gmail.com>
2017-12-28 02:51:43 +0530

    Push

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import glob
 batch_size = 64  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 5000  # Number of samples to train on.
+num_samples = 2000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = 'hin.txt'
 
